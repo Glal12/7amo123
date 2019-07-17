aa373b05511fcd05b9cc28345f472478f602a0cb
7amo12 <51268200+7amo12@users.noreply.github.com>
2019-07-18 01:17:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '-'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`-help`)
+client.user.setGame(`-bc`)
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
