1ca6f2eeacfcdb8e597d1f17592ef75c456583e7
7amo12 <51268200+7amo12@users.noreply.github.com>
2019-07-18 01:16:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,111 +34,49 @@ client.user.setGame(`-help`)
 
 
 
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
-
-
-
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
-
-client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix + "help") {
-     message.channel.send('**The Message Was Sent On Private**')
-	
-		 
-
-
- message.author.sendMessage(`
- **
-╔[❖════════════❖]╗
-                 Prefix = ' - '
-╚[❖════════════❖]╝
-
-╔[❖════════════❖]╗
-                      Commands
-╚[❖════════════❖]╝
-
-
-[❖═════════════════════════════❖]
-
- ❖ ${prefix}bc <message> ➾ Message All Members In Server
-
- ❖ ${prefix}obc <message> ➾ Message To Online Members
-
- ❖ ${prefix}ebc <message>  ➾ Embed Broadcast
-
- ❖ ${prefix}rbc <message> ➾ Message To Who Have Role
-
- ❖ ${prefix}inv ➾ Link For Invite Me To Your Server
-  
- ❖ ${prefix}bot ➾ Info Bot
- 
- ❖ ${prefix}support ➾ Invite To Server Made Me
- 
- ❖ ${prefix}ping ➾ Test My Ping
-
- ❖ ${prefix}allbots ➾ All Bots In Server
-
- [❖═════════════════════════════❖]
-
-Support server : ||||
-
-[❖═════════════════════════════❖]
-
-Server Made Me : Jokars Shop
-
-[❖═════════════════════════════❖]
- 
-<@484361588766146560> OWNER
-
-[❖═════════════════════════════❖]
-
-I Am In ${client.guilds.size} Servers
-
-[❖═════════════════════════════❖]
-
-virson:0.0.7
-
-[❖═════════════════════════════❖]
-
-My Invite Link : ||||
-
-[❖═════════════════════════════❖]
-**
-
-
-`);
-
+lient.on('message', message => {
+  if(message.content.startsWith(prefix + 'bc')) {
+  let args = message.content.split(" ").slice(1).join(" ");
+  if(!args) return message.channel.send(`**:rolling_eyes: please type the broadcast message**`)
+  let filter = m => m.author.id == message.author.id
+  let broadcastt = new Discord.RichEmbed()
+  .setColor('#36393e')
+  .addField(`**[1] broadcast for all members\n\n[2] broadcast for online members\n\n[0] to cansel**`,`** **`)
+  message.channel.send(broadcastt).then(msg => {
+  message.channel.awaitMessages(filter, {
+    max: 1,
+    time: 90000,
+    errors: ['time']
+  })
+  .then(collected => {
+    if(collected.first().content === '1') {
+      message.channel.bulkDelete(1)
+  message.channel.send(`**Broadcast being sent to ${message.guild.members.size} members ...**`);
+  msg.delete()
+     return message.guild.members.forEach(m => {
+  m.send(args.replace('[user]', m))
+      })
+  }
+  if(collected.first().content === '2') {
+    msg.delete()
+    message.channel.bulkDelete(1)
+    message.channel.send(`**Broadcast being sent to ${message.guild.members.filter(m=>m.presence.status == 'online').size} members ...**`);
+  message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
+  message.guild.members.filter(m => m.presence.status === 'dnd').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
+  return message.guild.members.filter(m => m.presence.status === 'idle').forEach(m => {
+    m.send(args.replace('[user]', m))
+  })
     }
-});
+  if(collected.first().content === '0') {
+    message.channel.bulkDelete(1)
+    msg.delete()
+    return message.channel.send(`**Broadcast Has Been Canceled**`);
+  }})})}
+  });
 
 
 
@@ -147,7 +85,6 @@ My Invite Link : ||||
 
 
 
- 
 
 
 
@@ -158,239 +95,4 @@ My Invite Link : ||||
 
 
 
-client.on('message', message => {
-  if(!message.channel.guild) return;
-if (message.content.startsWith('-ping')) {
-if(!message.channel.guild) return;
-var msg = `${Date.now() - message.createdTimestamp}`
-var api = `${Math.round(client.ping)}`
-if (message.author.bot) return;
-let embed = new Discord.RichEmbed()
-.setAuthor(message.author.username,message.author.avatarURL)
-.setColor('RANDOM')
-.addField('**Time Taken:**',msg + " ms 📶 ")
-.addField('**Discord API:**',api + " ms 📶 ")
-message.channel.send({embed:embed});
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix + "baned")) {
-        message.guild.fetchBans()
-        .then(bans => message.channel.send(`:small_orange_diamond: **Server Ban List :** ${bans.size} `))
-  .catch(console.error);
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-                                        if (message.content === "-support") {
-                                        let embed = new Discord.RichEmbed()
-                                     .setAuthor(message.author.username)
-                                     .setColor("#9B59B6")
-                                     .addField(" ** :gear: Server Support :gear: **" , "  ** https://discord.gg/hFKuUKa**")
-                                        
-                                        
-                                     message.channel.sendEmbed(embed);
-                                       }
-                                   });
-
-
-
-
-client.on('message', message => { 
-    if (message.content.startsWith(prefix + "bot")) { 
-    message.channel.send({ 
-        embed: new Discord.RichEmbed() 
-            .setAuthor(client.user.username,client.user.avatarURL) 
-            .setThumbnail(client.user.avatarURL) 
-            .setColor('RANDOM') 
-            .setTitle('Info TheJokar Bots.') 
-            .addField('**My Ping**' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true) 
-            .addField('**RAM Usage**', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true) 
-            .addField('**Servers**', [client.guilds.size], true) 
-            .addField('**Channels**' , `[ ${client.channels.size} ]` , true) 
-            .addField('**Users**' ,`[ ${client.users.size} ]` , true) 
-            .addField('**My Name**' , `[ ${client.user.tag} ]` , true) 
-            .addField('**My ID**' , `[ ${client.user.id} ]` , true) 
-            .addField('**DiscordJS**' , `[ ${Discord.version} ]` , true) 
-            .addField('**NodeJS**' , `[ ${process.version} ]` , true) 
-            .addField('**Arch**' , `[ ${process.arch} ]` , true) 
-            .addField('**Platform**' , `[ ${process.platform} ]` , true) 
-                  .addField('**My Prefix**' , `[ ${prefix} ]` , true) 
-                  .addField('**My Language**' , `[ Java Script ]` , true)
-                  .setFooter('${client.user.tag}') 
-    }) 
-} 
-}); 
-
-
-
-client.on('message' , message => {
-      if(message.author.bot) return;
-     
-      if(message.content.startsWith(prefix + "rbc")) {
-        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-        let args = message.content.split(" ").slice(2);
-     var codes = args.join(' ')
-       
-        if(!codes) {
-          message.channel.send("Try -help")
-            return;
-        }
-     
-     
-              var role = message.mentions.roles.first();
-                if(!role) {
-                  message.reply("I CAnot Find TheRole")
-                    return;
-                }
-            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
-              n.send(
-              "**" + "السيرفر :" + "\n" +
-              `${message.guild.name}` + "\n" +
-              "المرسل :" + "\n" +
-              `${message.author.tag}` + "\n" +
-              "الرسالة :" + "\n" +
-              `${codes}` + "**"
-              )
-            })
-            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
-        }
-    });
-	
-	
-	
-	
-    client.on('message', message => {
-	    var prefix = "-";
-              if(!message.channel.guild) return;
-    if(message.content.startsWith(prefix + 'ebc')) {
-    if(!message.channel.guild) return message.channel.send('**This Command Only For Servers**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Dont Have perms** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Speed Bot";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**Write Some Things To Broadcast**');message.channel.send(`**Are You Sure \nThe Broadcast: ** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-    
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-    
-    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
-    message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent To __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-  
-  var bc = new
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setTitle('Broadcast')
-       .addField('Server', message.guild.name)
-       .addField('Sender', message.author.username)
-       .addField('Message', args)
-       .setThumbnail(message.author.avatarURL)
-       .setFooter(copy, client.user.avatarURL);
-    m.send({ embed: bc })
-    msg.delete();
-    })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
-    }
-    });
-
-
-
-
-client.on("message", async message => {
-    if(message.content.startsWith(prefix + "inv")) {
-        let invite = new Discord.RichEmbed()
-            .setColor("RANDOM")
-            .setAuthor(message.author.username, message.author.displayAvatarURL)
-            .setThumbnail(message.author.avatarURL)
-            .setTitle("**Click Here To Invite The Bot To Your Server :sparkling_heart:**")
-            .setURL(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`);
-            message.channel.sendEmbed(invite);
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.login(process.env.NjAwNzMxNzA1MTQ1Mjk0ODYy.XS-paQ.6r6PM-oxUsg4AQCmOpl2UI-5x4U);
+client.login(process.env.BOT_TOKEN);
